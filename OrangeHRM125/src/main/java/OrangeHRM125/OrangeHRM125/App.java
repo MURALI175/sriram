d5384716007ef4b9cc92a1df59d208992130926c
Lenovo <Lenovo@DESKTOP-4U48RM4>
2022-12-22 15:06:46 +0530

    ram

---
+++ OrangeHRM125/src/main/java/OrangeHRM125/OrangeHRM125/App.java
@@ -0,0 +1,13 @@
+package OrangeHRM125.OrangeHRM125;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
